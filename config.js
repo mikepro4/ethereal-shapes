a1504702fbee19b99fa3b2e6b52da1049a6e2e5c
Mikhail <pmp@nopreset.ru>
2022-02-05 12:30:20 -0500

    final

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 
-  export const nftMarketAddress = "0x9E545E3C0baAB3E08CdfD552C960A1050f373042"
-  export const nftAddress = "0xa82fF9aFd8f496c3d6ac40E2a0F282E47488CFc9"
+  export const nftMarketAddress = "0x8f86403A4DE0BB5791fa46B8e795C547942fE4Cf"
+  export const nftAddress = "0x9d4454B023096f34B160D6B654540c56A1F81688"
