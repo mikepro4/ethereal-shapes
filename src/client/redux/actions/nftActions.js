d4708574003b7a33ec370d5daf722322c99360d1
Mikhail <pmp@nopreset.ru>
2022-02-14 16:33:12 -0500

    test

--- src/client/redux/actions/nftActions.js
+++ src/client/redux/actions/nftActions.js
@@ -393,7 +393,7 @@ export const searchNFTs = (type, identifier, offset, limit, query, success) => a
         }
     }
 
-    const token = localStorage.getItem('token');
+    // const token = localStorage.getItem('token');
 
     await api
         .post("/NFTs/search", {
@@ -402,7 +402,7 @@ export const searchNFTs = (type, identifier, offset, limit, query, success) => a
             offset: offset,
             limit: limit,
             order: sortDirection,
-            user: "token"
+            user: ""
         })
         .then(response => {
             if (success) {
